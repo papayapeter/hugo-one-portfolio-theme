bb7ae1cbe9f31accdf51ebd5105a75d5ebd56dd0
papayapeter <t.babel@gmx.com>
2020-04-29 13:33:35 +0200

    improved menu

--- template/one-portfolio/js/main.js
+++ template/one-portfolio/js/main.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
       $('img', this).attr('src', $('img', this).attr('src').replace('animated.gif', 'static.gif'));
     }
   );
+
+  // to open nav menu on small screens
+  $('.navbar-toggler').click(function() {
+    $('.menu-modal').toggleClass('menu-active');
+    $('span', this).toggleClass('fa-bars');
+    $('span', this).toggleClass('fa-times');
+    $('body').toggleClass('menu-active')
+  });
 });
